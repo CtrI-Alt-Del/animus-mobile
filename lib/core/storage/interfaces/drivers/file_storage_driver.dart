57d4a97f7b7207e92e1e3a3d23459bb99df9b447
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-03-23 21:41:38 -0300

    fix: ANI-33 address review feedback for core contracts and theme

--- lib/core/storage/interfaces/drivers/file_storage_driver.dart
+++ lib/core/storage/interfaces/drivers/file_storage_driver.dart
@@ -5,6 +5,6 @@ import 'package:animus_mobile/core/storage/dtos/upload_url_dto.dart';
 abstract class FileStorageDriver {
   String getFileUrl(String filePath);
   Future<void> uploadFile(File file, UploadUrlDto uploadUrl);
-  Future<void> uploadFiles(List<File> files, List<UploadUrlDto> uploadUrls);
+  Future<void> uploadFiles(Map<File, UploadUrlDto> uploads);
   Future<File> downloadFile(String filePath);
 }
