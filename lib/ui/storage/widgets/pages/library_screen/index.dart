dbc6555ed55b92c345c43e18b2589d724b53f1d4
0thigs <machinimavlogs@gmail.com>
2026-04-28 00:39:12 -0300

    feat: ANI-72 Update index.dart to export additional components for LibraryScreen

--- lib/ui/storage/widgets/pages/library_screen/index.dart
+++ lib/ui/storage/widgets/pages/library_screen/index.dart
@@ -1,3 +1,6 @@
 import 'package:animus/ui/storage/widgets/pages/library_screen/library_screen_view.dart';
+export 'package:animus/ui/storage/widgets/pages/library_screen/library_screen_presenter.dart';
+export 'package:animus/ui/storage/widgets/pages/library_screen/components/folder_list_item.dart';
+export 'package:animus/ui/storage/widgets/pages/library_screen/create_folder_modal/create_folder_modal_view.dart';
 
 typedef LibraryScreen = LibraryScreenView;
