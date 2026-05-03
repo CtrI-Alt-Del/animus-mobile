de19cc5455b06e8671416716b951d1954b97d53e
0thigs <machinimavlogs@gmail.com>
2026-05-03 20:05:07 -0300

    feat: ANI-82 redesign library screen with tabs and folder grid

---
+++ lib/ui/storage/widgets/pages/library_screen/folder_grid_card/index.dart
@@ -0,0 +1,3 @@
+import 'package:animus/ui/storage/widgets/pages/library_screen/folder_grid_card/folder_grid_card_view.dart';
+
+typedef FolderGridCard = FolderGridCardView;
--- lib/ui/storage/widgets/pages/library_screen/index.dart
+++ lib/ui/storage/widgets/pages/library_screen/index.dart
@@ -1,7 +1,9 @@
 import 'package:animus/ui/storage/widgets/pages/library_screen/library_screen_view.dart';
 export 'package:animus/ui/storage/widgets/pages/library_screen/library_screen_presenter.dart';
 export 'package:animus/ui/storage/widgets/pages/library_screen/create_folder_modal/index.dart';
-export 'package:animus/ui/storage/widgets/pages/library_screen/folder_list_item/index.dart';
-export 'package:animus/ui/storage/widgets/pages/library_screen/unfoldered_card/index.dart';
+export 'package:animus/ui/storage/widgets/pages/library_screen/folder_grid_card/index.dart';
+export 'package:animus/ui/storage/widgets/pages/library_screen/library_tabs/index.dart';
+export 'package:animus/ui/storage/widgets/pages/library_screen/new_folder_button/index.dart';
+export 'package:animus/ui/storage/widgets/pages/library_screen/unfoldered_analysis_tile/index.dart';
 
 typedef LibraryScreen = LibraryScreenView;
