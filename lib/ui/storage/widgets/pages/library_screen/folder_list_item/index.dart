7172a7c70eb47bf70710ce5878fe82227d60a156
0thigs <machinimavlogs@gmail.com>
2026-05-03 20:05:07 -0300

    feat: ANI-82 redesign library screen with tabs and folder grid

--- lib/ui/storage/widgets/pages/library_screen/folder_list_item/index.dart
+++
@@ -1,3 +0,0 @@
-import 'package:animus/ui/storage/widgets/pages/library_screen/folder_list_item/folder_list_item_view.dart';
-
-typedef FolderListItem = FolderListItemView;
---
+++ lib/ui/storage/widgets/pages/library_screen/library_tabs/library_tabs_view.dart
@@ -0,0 +1,70 @@
+import 'package:animus/theme.dart';
+import 'package:flutter/material.dart';
+
+class LibraryTabsView extends StatelessWidget {
+  static const List<String> _labels = <String>['Todas', 'Pastas'];
+
+  final int selectedIndex;
+  final ValueChanged<int> onSelected;
+
+  const LibraryTabsView({
+    super.key,
+    required this.selectedIndex,
+    required this.onSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final AppThemeTokens tokens =
+        Theme.of(context).extension<AppThemeTokens>() ?? AppTheme.tokens;
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      padding: const EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        color: tokens.surfaceCard,
+        borderRadius: BorderRadius.circular(14),
+        border: Border.all(color: tokens.borderSubtle),
+      ),
+      child: Row(
+        children: List<Widget>.generate(_labels.length, (int index) {
+          final bool isSelected = index == selectedIndex;
+          return Expanded(
+            child: GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () => onSelected(index),
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 180),
+                curve: Curves.easeOut,
+                height: 44,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  gradient: isSelected
+                      ? LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: <Color>[
+                            tokens.accent,
+                            tokens.accentStrong,
+                          ],
+                        )
+                      : null,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text(
+                  _labels[index],
+                  style: textTheme.labelMedium?.copyWith(
+                    color: isSelected
+                        ? tokens.surfacePage
+                        : tokens.textSecondary,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
