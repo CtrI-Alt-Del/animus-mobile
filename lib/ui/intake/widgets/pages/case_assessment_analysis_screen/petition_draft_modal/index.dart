22cbacddebe0c818bf1b43f7e290acaacaee3c46
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-06-09 14:20:47 -0300

    chore: ANI-199 update dependencies, platform files, and remove obsolete petition draft modal

--- lib/ui/intake/widgets/pages/case_assessment_analysis_screen/petition_draft_modal/index.dart
+++
@@ -1,3 +0,0 @@
-import 'package:animus/ui/intake/widgets/pages/case_assessment_analysis_screen/petition_draft_modal/petition_draft_modal_view.dart';
-
-typedef PetitionDraftModal = PetitionDraftModalView;
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import file_picker
 import google_sign_in_ios
 import package_info_plus
 import printing
+import share_plus
 import shared_preferences_foundation
 import url_launcher_macos
 
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
